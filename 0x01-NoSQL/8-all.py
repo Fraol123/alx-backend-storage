f8946bd9bc5779a2e7a467e83928cc4b5408b8d1
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-12-14 19:20:39 +0300

    fixed pep error

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,10 +6,10 @@ import pymongo
 
 
 def list_all(mongo_collection):
-	"""
-	function to list all doc in a collection
-	"""
-	if not mongo_collection:
-		return []
-	documents = mongo_collection.find()
-	return [post for post in documents]
+    """
+    function to list all doc in a collection
+    """
+    if not mongo_collection:
+        return []
+    documents = mongo_collection.find()
+    return [post for post in documents]
